1c5536901b1f6bd72e5eb25d0764386e7384427c
Altynbekovzarlyk <altynbekov.zarlyk7@gmail.com>
2022-12-18 19:38:59 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Введите имя для списка А: ");
+        for (int i = 0; i <= 4; i++) {
+            name.add(scanner.nextLine());
+        }
+        System.out.println("Сиписок: " + name);
+        System.out.println("--------------------");
+
+        ArrayList<String> name2 = new ArrayList<>();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Введите имя для списка Б: ");
+        for (int i = 0; i <= 4; i++) {
+            name2.add(scanner2.nextLine());
+        }
+        System.out.println("Сиписок: " + name2);
+
+        ArrayList<String> name3 = new ArrayList<>();
+        name3.addAll(name);
+        name3.add(1,name2.get(4));
+        name3.add(3,name2.get(3));
+        name3.add(5,name2.get(2));
+        name3.add(7,name2.get(1));
+        name3.add(9,name2.get(0));
+
+        System.out.println(name3);
+        Collections.sort(name3, Comparator.comparing(String::length));
+        System.out.println(name3);
+    }
+}
